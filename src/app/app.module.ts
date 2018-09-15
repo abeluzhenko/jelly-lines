338ef66d7acac67000c45a0819186b1b8f997036
Anton Beluzhenko <peregrimm@gmail.com>
2018-09-15 10:12:45 +0300

    Initialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BallComponent } from './ball/ball.component';
+import { CellComponent } from './cell/cell.component';
+import { GridComponent } from './grid/grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BallComponent,
+    CellComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule
