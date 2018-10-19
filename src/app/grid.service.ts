217fbef856237317b740874704940444988d3860
Anton Beluzhenko <peregrimm@gmail.com>
2018-10-19 16:42:57 +0300

    Add animation$ observable

--- src/app/grid.service.ts
+++ src/app/grid.service.ts
@@ -2,12 +2,22 @@ import { Injectable } from '@angular/core';
 import { BallState, BallColors, BallColor } from './ball/ball.model';
 import { ICell } from './cell/cell.model';
 import { Subject, Observable, merge, pipe } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
-import { Path } from './path.model';
+import { filter, map, tap } from 'rxjs/operators';
+import { Path, IPathCell } from './path.model';
 
 export interface IGridInput {
   cells: ICell[];
   cell?: ICell;
+}
+
+export enum GridAnimationType {
+  Move = 0,
+  Match = 1,
+}
+
+export interface IGridAnimation {
+  type: GridAnimationType;
+  cells: ICell[];
 }
 
 export const DEFAULT_NEW_BALLS_COUNT = 3;
@@ -18,9 +28,11 @@ export const DEFAULT_NEW_BALLS_COUNT = 3;
 export class GridService {
 
   private _outputSubject: Subject<ICell[]> = new Subject<ICell[]>();
+  private _animationSubject: Subject<IGridAnimation> = new Subject<IGridAnimation>();
 
   public input$: Subject<IGridInput> = new Subject<IGridInput>();
   public output$: Observable<ICell[]>;
+  public animation$: Observable<IGridAnimation>;
 
   public static getRandomColor(): BallColor {
     return BallColors[Math.floor(BallColors.length * Math.random())];
@@ -44,9 +56,10 @@ export class GridService {
 
   constructor() {
     this.output$ = this._outputSubject.asObservable();
+    this.animation$ = this._animationSubject.asObservable();
 
     const turn$ = this.getTurnObservable(this.input$);
-    const move$ = this.getMoveObservable(this.input$);
+    const move$ = this.getMoveObservable(this.input$, this._animationSubject);
     const activate$ = this.getActivateObservable(this.input$);
 
     merge(
@@ -79,18 +92,19 @@ export class GridService {
     );
   }
 
-  private getMoveObservable(input$: Observable<IGridInput>): Observable<IGridInput> {
+  private getMoveObservable(
+    input$: Observable<IGridInput>,
+    animationSubject: Subject<IGridAnimation>
+  ): Observable<IGridInput> {
     return input$.pipe(
       filter(data => data.cell && !data.cell.ball),
       map(data => {
         const activeCell = data.cells
-          .filter(c => c.ball)
-          .filter(c => c.ball.state === BallState.active)
-          .pop();
+          .filter(c => c.ball && c.ball.state === BallState.active)[0];
         // If we have an active ball
         // check if the ball can be moved to the cell
         if (!activeCell) {
-          return data;
+          return { data, path: null };
         }
         const pathGrid = Path.getPathGrid(data.cells);
         const path = Path.getPath(
@@ -101,15 +115,23 @@ export class GridService {
 
         // No path - return the current cell
         if (!path.length) {
-          return data;
+          return { data, path: null };
         }
         // Move the ball and return the updated grid
         const ballToMove = Object.assign(activeCell.ball,
           { id: data.cell.id, state: BallState.idle });
         delete data.cells[activeCell.id].ball;
         data.cells[data.cell.id].ball = ballToMove;
-        return data;
-      }));
+        return { data, path };
+      }),
+      tap((data: { data: IGridInput, path: ICell[] }) =>
+        animationSubject.next({
+          type: GridAnimationType.Move,
+          cells: data.path
+        }
+      )),
+      map((data: { data: IGridInput, path: ICell[] }) => data.data)
+    );
   }
 
   private getActivateObservable(input$: Observable<IGridInput>): Observable<IGridInput> {
