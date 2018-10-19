1051bec37ed91f1023da1ac6cfe9dda3e1dc0126
Anton Beluzhenko <peregrimm@gmail.com>
2018-10-19 17:11:33 +0300

    Add grid model initial tests

---
+++ src/app/grid.model.spec.ts
@@ -0,0 +1,11 @@
+describe('Grid module', () => {
+  it('getGrid should return a proper grid', () => {
+
+  });
+  it('getRandomGrid should return a proper randomized grid', () => {
+
+  });
+  it('getRandomColor should return a random color from the colors set', () => {
+
+  });
+});
