7ecbbd612cd22d879cd331d99a32042880f5e0a9
Anton Beluzhenko <peregrimm@gmail.com>
2018-10-19 08:34:07 +0300

    Wrap path functions with a class

--- src/app/path.model.spec.ts
+++ src/app/path.model.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { getClosestCell, PathCell, getAdjacent, getDistance, makePath, getPathGrid, getPath } from './path.model';
+import { Path, IPathCell } from './path.model';
 import { ICell } from './cell/cell.model';
 import { BallState, BallColor } from './ball/ball.model';
 
@@ -13,7 +13,7 @@ describe('Path module', () => {
     for (let i = 0; i < 100; i++) {
       cells.push({ id: i });
     }
-    let grid: PathCell[] = getPathGrid(cells);
+    let grid: IPathCell[] = Path.getPathGrid(cells);
     expect(grid).toBeTruthy();
     expect(grid.length).toBe(100);
     expect(grid[50].order).toBe(0);
@@ -24,46 +24,46 @@ describe('Path module', () => {
       state: BallState.idle,
       color: BallColor.purple
     };
-    grid = getPathGrid(cells);
+    grid = Path.getPathGrid(cells);
     expect(grid[50].cost).toBe(Number.POSITIVE_INFINITY);
   });
 
   it('getClosest cells should return the closest cell', () => {
-    const cells: Set<PathCell> = new Set();
+    const cells: Set<IPathCell> = new Set();
     for (let i = 0; i < 10; i++) {
       cells.add({
         index: i,
         cost: i,
       });
     }
-    expect(getClosestCell(cells)).toEqual({ index: 0, cost: 0 });
+    expect(Path.getClosestCell(cells)).toEqual({ index: 0, cost: 0 });
     cells.add({ index: 10, cost: -1 });
-    expect(getClosestCell(cells)).toEqual({ index: 10, cost: -1 });
+    expect(Path.getClosestCell(cells)).toEqual({ index: 10, cost: -1 });
     cells.add({ index: 11, cost: Number.NEGATIVE_INFINITY });
-    expect(getClosestCell(cells)).toEqual({ index: 11, cost: Number.NEGATIVE_INFINITY });
+    expect(Path.getClosestCell(cells)).toEqual({ index: 11, cost: Number.NEGATIVE_INFINITY });
   });
 
   it('getAdjacent should return all the adjacent to the input cell', () => {
-    const grid: PathCell[] = [];
+    const grid: IPathCell[] = [];
     for (let i = 0; i < 100; i++) {
       grid.push({ index: i });
     }
 
     const checkCorner = (cell, n1, n2) => {
-      const adjacent = getAdjacent(cell, grid, 10);
+      const adjacent = Path.getAdjacent(cell, grid, 10);
       expect(adjacent.length).toBe(2);
       expect(adjacent).toContain(n1);
       expect(adjacent).toContain(n2);
     };
     const checkSide = (cell, n1, n2, n3) => {
-      const adjacent = getAdjacent(cell, grid, 10);
+      const adjacent = Path.getAdjacent(cell, grid, 10);
       expect(adjacent.length).toBe(3);
       expect(adjacent).toContain(n1);
       expect(adjacent).toContain(n2);
       expect(adjacent).toContain(n3);
     };
     const checkOther = (cell, n1, n2, n3, n4) => {
-      const adjacent = getAdjacent(cell, grid, 10);
+      const adjacent = Path.getAdjacent(cell, grid, 10);
       expect(adjacent.length).toBe(4);
       expect(adjacent).toContain(n1);
       expect(adjacent).toContain(n2);
@@ -94,34 +94,34 @@ describe('Path module', () => {
     checkOther(grid[55], grid[54], grid[56], grid[65], grid[45]);
 
     // Invalid input
-    expect(getAdjacent({ index: Number.POSITIVE_INFINITY }, grid)).toEqual([]);
-    expect(getAdjacent({ index: Number.NEGATIVE_INFINITY }, [])).toEqual([]);
+    expect(Path.getAdjacent({ index: Number.POSITIVE_INFINITY }, grid)).toEqual([]);
+    expect(Path.getAdjacent({ index: Number.NEGATIVE_INFINITY }, [])).toEqual([]);
   });
 
   it('getDistance should return a correct Manhatan distance value', () => {
-    expect(getDistance({ index: 0 }, { index: 22 }, 10)).toBe(4);
-    expect(getDistance({ index: 0 }, { index: 99 }, 10)).toBe(18);
-    expect(getDistance({ index: 10 }, { index: 11 }, 10)).toBe(1);
-    expect(getDistance({ index: 50 }, { index: 50 }, 10)).toBe(0);
-    expect(getDistance({ index: 0 }, { index: 9 }, 10)).toBe(9);
+    expect(Path.getDistance({ index: 0 }, { index: 22 }, 10)).toBe(4);
+    expect(Path.getDistance({ index: 0 }, { index: 99 }, 10)).toBe(18);
+    expect(Path.getDistance({ index: 10 }, { index: 11 }, 10)).toBe(1);
+    expect(Path.getDistance({ index: 50 }, { index: 50 }, 10)).toBe(0);
+    expect(Path.getDistance({ index: 0 }, { index: 9 }, 10)).toBe(9);
 
     // Invalid input
-    expect(getDistance({ index: undefined }, { index: undefined }, 10)).toBeNaN();
-    expect(getDistance({ index: 0 }, { index: undefined }, 10)).toBeNaN();
-    expect(getDistance({ index: Number.POSITIVE_INFINITY }, { index: Number.NEGATIVE_INFINITY }, 10)).toBeNaN();
+    expect(Path.getDistance({ index: undefined }, { index: undefined }, 10)).toBeNaN();
+    expect(Path.getDistance({ index: 0 }, { index: undefined }, 10)).toBeNaN();
+    expect(Path.getDistance({ index: Number.POSITIVE_INFINITY }, { index: Number.NEGATIVE_INFINITY }, 10)).toBeNaN();
   });
 
   it('makePath should return a correct path array', () => {
-    const grid: PathCell[] = [];
+    const grid: IPathCell[] = [];
     for (let i = 0; i < 100; i++) {
       grid.push({ index: i, order: i + 1 });
     }
-    let path = makePath(grid[9], grid, getAdjacent, 0, 10);
+    let path = Path.makePath(grid[9], grid, Path.getAdjacent, 0, 10);
     expect(path.length).toBe(10);
     expect(path[0]).toBe(grid[0]);
     expect(path[9]).toBe(grid[9]);
 
-    path = makePath(grid[99], grid, getAdjacent, 0, 10);
+    path = Path.makePath(grid[99], grid, Path.getAdjacent, 0, 10);
     expect(path.length).toBe(0);
 
     const p = [ 0, 1, 2, 12, 22, 21, 20 ];
@@ -133,32 +133,32 @@ describe('Path module', () => {
       }
       grid[i].order = 0;
     }
-    path = makePath(grid[20], grid, getAdjacent, 0, 10);
+    path = Path.makePath(grid[20], grid, Path.getAdjacent, 0, 10);
     expect(path.length).toBe(7);
     expect(path.map(el => el.index)).toEqual(p);
   });
 
   it('getPath should return the shortest path between two points', () => {
-    const grid: PathCell[] = [];
+    const grid: IPathCell[] = [];
     for (let i = 0; i < 100; i++) {
       grid.push({ index: i, cost: 0, order: 0 });
     }
-    let path = getPath(grid[0], grid[99], grid, 0, 10);
+    let path = Path.getPath(grid[0], grid[99], grid, 0, 10);
     expect(path).not.toEqual([]);
     expect(path.length).toBe(19);
 
     for (let i = 0; i < 10; i++) {
       grid[10 + i].cost = Number.POSITIVE_INFINITY;
     }
-    path = getPath(grid[0], grid[20], grid, 0, 10);
+    path = Path.getPath(grid[0], grid[20], grid, 0, 10);
     expect(path).toEqual([]);
 
     grid[19].cost = 1;
-    path = getPath(grid[0], grid[20], grid, 0, 10);
+    path = Path.getPath(grid[0], grid[20], grid, 0, 10);
     expect(path).not.toEqual([]);
     expect(path.length).toBe(21);
 
-    path = getPath(grid[0], grid[1], grid, 0, 10);
+    path = Path.getPath(grid[0], grid[1], grid, 0, 10);
     expect(path).not.toEqual([]);
     expect(path.length).toBe(2);
   });
--- src/app/path.model.ts
+++ src/app/path.model.ts
@@ -1,6 +1,6 @@
 export const GRID_SIZE = 9;
 
-export interface PathCell {
+export interface IPathCell {
   index: number;
   cost?: number;
   order?: number;
@@ -8,125 +8,133 @@ export interface PathCell {
   isEnd?: boolean;
 }
 
-export const getPathGrid = cells => cells.map((cell, index) => ({
-  index,
-  order: 0,
-  cost: cell.ball ? Number.POSITIVE_INFINITY : 1
-}));
+export class Path {
+  public static getPathGrid(cells): IPathCell[] {
+    return cells.map((cell, index) => ({
+      index,
+      order: 0,
+      cost: cell.ball ? Number.POSITIVE_INFINITY : 1
+    }));
+  }
 
-export const getClosestCell = (cells: Set<PathCell>): PathCell => Array
-  .from(cells)
-  .reduce((min, current) => current.cost < min.cost ? current : min,
-    { cost: Number.POSITIVE_INFINITY } as PathCell);
+  public static getClosestCell(cells: Set<IPathCell>): IPathCell {
+    return Array
+      .from(cells)
+      .reduce(
+        (min, current) => current.cost < min.cost ? current : min,
+        { cost: Number.POSITIVE_INFINITY } as IPathCell
+      );
+  }
 
-export const getAdjacent = (
-  cell: PathCell,
-  grid: PathCell[],
-  gridSize = GRID_SIZE
-): PathCell[] => {
-  const result = [];
-  if (cell.index < 0 || cell.index >= gridSize * gridSize) {
-    return result;
-  }
-  const x = Math.floor(cell.index / gridSize);
-  const y = cell.index % gridSize;
-  if (x - 1 >= 0) {
-    result.push(grid[(x - 1) * gridSize + y]);
-  }
-  if (x + 1 < gridSize) {
-    result.push(grid[(x + 1) * gridSize + y]);
-  }
-  if (y - 1 >= 0) {
-    result.push(grid[x * gridSize + (y - 1)]);
-  }
-  if (y + 1 < gridSize) {
-    result.push(grid[x * gridSize + (y + 1)]);
-  }
-  return result.filter(el => el !== undefined);
-};
-
-export const getDistance = (
-  cell0: PathCell,
-  cell1: PathCell,
-  gridSize = GRID_SIZE
-): number => {
-  const [x0, y0] = [Math.floor(cell0.index / gridSize), cell0.index % gridSize];
-  const [x1, y1] = [Math.floor(cell1.index / gridSize), cell1.index % gridSize];
-  return Math.abs(x0 - x1) + Math.abs(y0 - y1);
-};
-
-export const makePath = (
-  from: PathCell,
-  grid: PathCell[],
-  adjacentFn: Function,
-  emptyCellValue = 0,
-  gridSize = GRID_SIZE
-) => {
-  const path = [from];
-  const index = from.order;
-  let current = from;
-  for (let i = index; i > 1; i--) {
-    const next = adjacentFn(current, grid, gridSize)
-      .filter(cell => cell.order !== emptyCellValue)
-      .filter(cell => cell.order === i - 1)[0];
-    if (!next) {
-      return [];
+  public static getAdjacent(
+    cell: IPathCell,
+    grid: IPathCell[],
+    gridSize = GRID_SIZE
+  ): IPathCell[] {
+    const result = [];
+    if (cell.index < 0 || cell.index >= gridSize * gridSize) {
+      return result;
     }
-    path.push(next);
-    current = next;
+    const x = Math.floor(cell.index / gridSize);
+    const y = cell.index % gridSize;
+    if (x - 1 >= 0) {
+      result.push(grid[(x - 1) * gridSize + y]);
+    }
+    if (x + 1 < gridSize) {
+      result.push(grid[(x + 1) * gridSize + y]);
+    }
+    if (y - 1 >= 0) {
+      result.push(grid[x * gridSize + (y - 1)]);
+    }
+    if (y + 1 < gridSize) {
+      result.push(grid[x * gridSize + (y + 1)]);
+    }
+    return result.filter(el => el !== undefined);
   }
-  return path.reverse();
-};
 
-export const getPath = (
-  from: PathCell,
-  to: PathCell,
-  grid: PathCell[],
-  emptyValue = 0,
-  gridSize = GRID_SIZE
-) => {
-
-  if (getDistance(from, to, gridSize) === 1) {
-    return [from, to];
+  public static getDistance(
+    cell0: IPathCell,
+    cell1: IPathCell,
+    gridSize = GRID_SIZE
+  ): number {
+    const [x0, y0] = [Math.floor(cell0.index / gridSize), cell0.index % gridSize];
+    const [x1, y1] = [Math.floor(cell1.index / gridSize), cell1.index % gridSize];
+    return Math.abs(x0 - x1) + Math.abs(y0 - y1);
   }
 
-  const opened = new Set();
-  const closed = new Set();
-  opened.add(from);
-  from.cost = 1;
-  let done = false;
-
-  const filterCell = cell => !closed.has(cell) && cell.cost < Number.POSITIVE_INFINITY;
-
-  while (opened.size && !done) {
-    const openedCell = getClosestCell(opened);
-    getAdjacent(openedCell, grid, gridSize)
-      .filter(cell => filterCell(cell))
-      .forEach(cell => {
-        // Add cell to the new list
-        opened.add(cell);
-        // If the current cell cost is higher
-        // than the new one then return
-        const cost = openedCell.order + 1 + getDistance(cell, to, gridSize);
-        if (cost <= cell.cost) {
-          return;
-        }
-        // Update cell cost
-        cell.cost = cost;
-        cell.order = openedCell.order + 1;
-        // If the current cell is the to then end loop
-        if (cell === to) {
-          done = true;
-          return;
-        }
-    });
-    // add current cell to the closed list
-    closed.add(openedCell);
-    // remove from the opened list
-    opened.delete(openedCell);
+  public static makePath(
+    from: IPathCell,
+    grid: IPathCell[],
+    adjacentFn: Function,
+    emptyCellValue = 0,
+    gridSize = GRID_SIZE
+  ) {
+    const path = [from];
+    const index = from.order;
+    let current = from;
+    for (let i = index; i > 1; i--) {
+      const next = adjacentFn(current, grid, gridSize)
+        .filter(cell => cell.order !== emptyCellValue)
+        .filter(cell => cell.order === i - 1)[0];
+      if (!next) {
+        return [];
+      }
+      path.push(next);
+      current = next;
+    }
+    return path.reverse();
   }
-  if (done) {
-    return [from, ...makePath(to, grid, getAdjacent, emptyValue, gridSize)];
+
+  public static getPath(
+    from: IPathCell,
+    to: IPathCell,
+    grid: IPathCell[],
+    emptyValue = 0,
+    gridSize = GRID_SIZE
+  ) {
+
+    if (Path.getDistance(from, to, gridSize) === 1) {
+      return [from, to];
+    }
+
+    const opened = new Set();
+    const closed = new Set();
+    opened.add(from);
+    from.cost = 1;
+    let done = false;
+
+    const filterCell = cell => !closed.has(cell) && cell.cost < Number.POSITIVE_INFINITY;
+
+    while (opened.size && !done) {
+      const openedCell = Path.getClosestCell(opened);
+      Path.getAdjacent(openedCell, grid, gridSize)
+        .filter(cell => filterCell(cell))
+        .forEach(cell => {
+          // Add cell to the new list
+          opened.add(cell);
+          // If the current cell cost is higher
+          // than the new one then return
+          const cost = openedCell.order + 1 + Path.getDistance(cell, to, gridSize);
+          if (cost <= cell.cost) {
+            return;
+          }
+          // Update cell cost
+          cell.cost = cost;
+          cell.order = openedCell.order + 1;
+          // If the current cell is the to then end loop
+          if (cell === to) {
+            done = true;
+            return;
+          }
+      });
+      // add current cell to the closed list
+      closed.add(openedCell);
+      // remove from the opened list
+      opened.delete(openedCell);
+    }
+    if (done) {
+      return [from, ...Path.makePath(to, grid, Path.getAdjacent, emptyValue, gridSize)];
+    }
+    return [];
   }
-  return [];
-};
+}
