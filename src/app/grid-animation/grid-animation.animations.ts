07eaa9efd7c516e81e69ceb336ed038bc14ad054
Anton Beluzhenko <peregrimm@gmail.com>
2018-10-31 14:22:09 +0300

    Refactor animation

--- src/app/grid-animation/grid-animation.animations.ts
+++ src/app/grid-animation/grid-animation.animations.ts
@@ -3,11 +3,42 @@ import {
   state,
   style,
   transition,
-  animate
+  animate,
+  keyframes,
+  AnimationMetadata,
 } from '@angular/animations';
 
 export const APPEAR_DURATION = 320;
+export const MATCH_DURATION = 500;
 export const MOVING_DURATION = 800;
+
+export function getMatchAnimation(
+  position: { x: number, y: number },
+  delay: number,
+  duration: number = MATCH_DURATION
+): AnimationMetadata[] {
+  const translate = `translate(${ position.x * 100 }%, ${ position.y * 100 }%)`;
+  return [
+    style({ transform: `${ translate } scale(1)`, opacity: 1 }),
+    animate(`${ duration }ms ${ delay + 1 }ms ease`, keyframes([
+      style({ transform: `${ translate } scale(1.6)`, opacity: 1, offset: 0.4 }),
+      style({ transform: `${ translate } scale(0)`, opacity: 1, offset: 1 }),
+    ])
+  )];
+}
+
+export function getMoveAnimation(
+  path: { x: number, y: number }[],
+  duration: number = MOVING_DURATION
+): AnimationMetadata {
+  const delta = duration / path.length;
+  const steps = path.map((p, i) => style({
+    transform: `translate(${ p.x * 100 }%, ${ p.y * 100 }%)`,
+    offset: delta * i / 1000,
+    opacity: 1,
+  }));
+  return animate(`${ duration }ms ease`, keyframes(steps));
+}
 
 export const cellBallAnimation = trigger('cellBallAnimation', [
   state(
--- src/app/grid-animation/grid-animation.component.ts
+++ src/app/grid-animation/grid-animation.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter, ViewChildren, QueryList
 import { AnimationStyleMetadata, style, keyframes, animate, AnimationBuilder } from '@angular/animations';
 import { IGridAnimation, GridAnimationType } from '../grid.service';
 import { BallComponent } from '../ball/ball.component';
-import { MOVING_DURATION } from './grid-animation.animations';
+import { MOVING_DURATION, getMatchAnimation, MATCH_DURATION, getMoveAnimation } from './grid-animation.animations';
 import { Grid } from '../grid.model';
 import { ICell } from '../cell/cell.model';
 import { IBall } from '../ball/ball.model';
@@ -30,16 +30,41 @@ export class GridAnimationComponent implements OnInit {
     if (!value) {
       return;
     }
-    if (value.type === GridAnimationType.Move) {
-      this.data = [ value.cells[value.cells.length - 1].ball ];
-      this._changeDetectorRef.detectChanges();
-      const animation = this.buildMoveAnimation(value.cells);
-      const player = animation.create(this.balls.first.elementRef.nativeElement);
-      player.onDone(() => {
-        this.complete.emit();
-        this.data = null;
-      });
-      player.play();
+    switch (value.type) {
+      case GridAnimationType.Move:
+        this.data = [ value.cells[value.cells.length - 1].ball ];
+        this._changeDetectorRef.detectChanges();
+
+        const moveAnimation = this.buildMoveAnimation(value.cells);
+        const movePlayer = moveAnimation.create(this.balls.first.elementRef.nativeElement);
+        movePlayer.onDone(() => {
+          movePlayer.destroy();
+          this.complete.emit();
+          this.data = null;
+        });
+        movePlayer.play();
+        break;
+      case GridAnimationType.Match:
+        this.data = value.cells.map(cell => cell.ball);
+        this._changeDetectorRef.detectChanges();
+        const doneList = [];
+
+        this.balls.forEach((item, i) => {
+          const matchAnimation = this.buildMatchAnimation(item.data.id, i * 100);
+          const matchPlayer = matchAnimation.create(item.elementRef.nativeElement);
+          const done = new Promise(resolve => {
+            matchPlayer.onDone(() => {
+              matchPlayer.destroy();
+              resolve();
+            });
+          });
+          doneList.push(done);
+          matchPlayer.play();
+        });
+        Promise.all(doneList).then(() => {
+          this.data = null;
+        });
+        break;
     }
   }
 
@@ -51,19 +76,14 @@ export class GridAnimationComponent implements OnInit {
   ngOnInit() {
   }
 
-  private buildMoveAnimation(data: ICell[], duration = MOVING_DURATION) {
-    const delta = duration / data.length;
-    const steps: AnimationStyleMetadata[] = data
-      .map((cell, i) => {
-        const position = Grid.getPosition(cell.id);
-        return style({
-          transform: `translate(${ position.x * 100 }%, ${ position.y * 100 }%)`,
-          offset: delta * i / 1000
-        });
-      });
-    return this._animationBuilder.build([
-      animate(`${ duration }ms ease`, keyframes(steps))
-    ]);
+  private buildMatchAnimation(id: number, delay: number, duration = MATCH_DURATION) {
+    const position = Grid.getPosition(id);
+    return this._animationBuilder.build(getMatchAnimation(position, delay, duration));
+  }
+
+  private buildMoveAnimation(path: ICell[], duration = MOVING_DURATION) {
+    const steps = path.map(cell => Grid.getPosition(cell.id));
+    return this._animationBuilder.build(getMoveAnimation(steps, duration));
   }
 
 }
