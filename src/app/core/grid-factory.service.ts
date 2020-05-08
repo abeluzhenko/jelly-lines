a3aab4fc662d7903288fbf4ea86b67c060b0174a
Anton Beluzhenko <anton@beluzhenko.ru>
2020-05-08 19:32:53 +0300

    feat(grid): implement state restoring

--- src/app/core/grid-factory.service.ts
+++ src/app/core/grid-factory.service.ts
@@ -5,7 +5,19 @@ import { GameState, INITIAL_STATE, cloneDeep } from './shared';
   providedIn: 'root'
 })
 export class GridFactoryService {
+  static STORAGE_ITEM_NAME = 'state';
+
   get initialState(): GameState {
+    const storageData = localStorage.getItem(GridFactoryService.STORAGE_ITEM_NAME);
+
+    if (storageData) {
+      try {
+        return JSON.parse(storageData);
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+
     return cloneDeep(INITIAL_STATE);
   }
 }
